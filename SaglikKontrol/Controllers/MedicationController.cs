5cb2012f4b4968d31bd7157da9c56fb70cfc801a
Ziya Ipek <ziya.ipek@hotmail.com>
2024-09-18 14:30:33 +0300

    Created contorllers

---
+++ SaglikKontrol/Controllers/MedicationController.cs
@@ -0,0 +1,16 @@
+﻿using Business.Models.Request.Create;
+using Business.Models.Request.Update;
+using Business.Models.Response;
+using Business.Services.Interface;
+using Infrastructure.Data.Postgres.Entities;
+using Web.Controllers.Base;
+
+namespace Web.Controllers
+{
+    public class MedicationController : BaseCRUDController<Medication, int, MedicationCreateDTO, MedicationUpdateDTO, MedicationResponseDTO>
+    {
+        public MedicationController(IMedicationService service) : base(service)
+        {
+        }
+    }
+}
---
+++ SaglikKontrol/Controllers/PatientDiseaseController.cs
@@ -0,0 +1,16 @@
+﻿using Business.Models.Request.Create;
+using Business.Models.Request.Update;
+using Business.Models.Response;
+using Business.Services.Interface;
+using Infrastructure.Data.Postgres.Entities;
+using Web.Controllers.Base;
+
+namespace Web.Controllers
+{
+    public class PatientDiseaseController : BaseCRUDController<PatientDisease, int, PatientDiseaseCreateDTO, PatientDiseaseUpdateDTO, PatientDiseaseResponseDTO>
+    {
+        public PatientDiseaseController(IPatientDiseaseService service) : base(service)
+        {
+        }
+    }
+}
