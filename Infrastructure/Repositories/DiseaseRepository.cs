50e228e09d81baf6629fe2625dbe11e2be4b1b85
Seras <72655616+emreozyoruk@users.noreply.github.com>
2024-09-18 11:28:10 +0300

    updated Unitofwork and Iunit and repos and Irepos

---
+++ Infrastructure/Repositories/DiseaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using Infrastructure.Data.Postgres.Entities;
+using Infrastructure.Data.Postgres.EntityFramework;
+using Infrastructure.Data.Postgres.Repositories.Base;
+using Infrastructure.Data.Postgres.Repositories.Interface;
+
+
+namespace Infrastructure.Data.Postgres.Repositories
+{
+    public class DiseaseRepository : Repository<Disease, int>, IDiseaseRepository
+    {
+        public DiseaseRepository(PostgresContext postgresContext) : base(postgresContext)
+        {
+        }
+    }
+}
+
+
